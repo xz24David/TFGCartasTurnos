42e44fefc3a14ece3753b228dabe0c94045dbe09
xz24David <xz24david@gmail.com>
2023-05-02 08:59:01 +0200

    Card ScriptableObject (mal)
    hay errores revertir

--- Assets/Script/CardDisplay.cs
+++ Assets/Script/CardDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
+
     public Card card;
 
     public Text nameText, EffectText, CostText;
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,23 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+namespace TJ
 {
-    int vidas;
-    bool pausa;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (vidas==0)//prueba del github
-        {
-            pausa = true;
-        }
-    }
+	public class GameManager : MonoBehaviour
+	{
+		public Character character;
+		public List<Card> playerDeck = new List<Card>();
+		public List<Card> cardLibrary = new List<Card>();
+		public List<Relic> relics = new List<Relic>();
+		public List<Relic> relicLibrary = new List<Relic>();
+		public int floorNumber = 1;
+		public int goldAmount;
+		PlayerStatsUI playerStatsUI;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		private void Awake()
+		{
+			playerStatsUI = FindObjectOfType<PlayerStatsUI>();
+		}
+		public void LoadCharacterStats()
+		{
+			relics.Add(character.startingRelic);
+			playerStatsUI.playerStatsUIObject.SetActive(true);
+			playerStatsUI.DisplayRelics();
+		}
+		public bool PlayerHasRelic(string rName)
+		{
+			foreach (Relic r in relics)
+			{
+				if (r.relicName == rName)
+					return true;
+			}
+			return false;
+		}
+		public void UpdateFloorNumber()
+		{
+			floorNumber += 1;
+
+			switch (floorNumber)
+			{
+				case 1:
+					playerStatsUI.floorText.text = floorNumber + "st Floor";
+					break;
+				case 2:
+					playerStatsUI.floorText.text = floorNumber + "nd Floor";
+					break;
+				case 3:
+					playerStatsUI.floorText.text = floorNumber + "rd Floor";
+					break;
+				default:
+					playerStatsUI.floorText.text = floorNumber + "th Floor";
+					break;
+			}
+		}
+		public void UpdateGoldNumber(int newGold)
+		{
+			goldAmount += newGold;
+			playerStatsUI.moneyAmountText.text = goldAmount.ToString();
+		}
+		public void DisplayHealth(int healthAmount, int maxHealth)
+		{
+			playerStatsUI.healthDisplayText.text = $"{healthAmount} / {maxHealth}";
+		}
+	}
 }
